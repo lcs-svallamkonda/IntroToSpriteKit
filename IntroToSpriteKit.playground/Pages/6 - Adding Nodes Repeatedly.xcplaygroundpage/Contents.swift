76d2a5f5edc0ff594ce052e41af38cec0e5913e4
Sunaina Vallamkonda <svallamkonda@lcs.on.ca>
2019-12-14 11:49:12 -0500

    made sand go into hourglass in 'adding nodes repeatedly' page

--- IntroToSpriteKit.playground/Pages/6 - Adding Nodes Repeatedly.xcplaygroundpage/Contents.swift
+++ IntroToSpriteKit.playground/Pages/6 - Adding Nodes Repeatedly.xcplaygroundpage/Contents.swift
@@ -57,8 +57,8 @@ func spawnSand() {
     // Vertical position just above top of the scene
     let y = scene.size.height - sand.size.height
 
-    // Horizontal position is random
-    let x = CGFloat.random(in: 0.0...scene.size.width)
+    // Horizontal position is random between 100 and 300
+    let x = CGFloat.random(in: 100...315)
 
     // Set the grain of sand's position
     sand.position = CGPoint(x: x, y: y)
@@ -90,12 +90,12 @@ func spawnSand() {
  
  */
 
-//// Add 100 sand particles, one each second, for the next 50 seconds
-//let actionSpawnSandParticle = SKAction.run(spawnSand)
-//let actionWait = SKAction.wait(forDuration: 0.5)
-//let sequenceSpawnThenWait = SKAction.sequence([actionSpawnSandParticle, actionWait])
-//let actionRepeatlyAddSand = SKAction.repeat(sequenceSpawnThenWait, count: 100)
-//scene.run(actionRepeatlyAddSand)
+// Add 100 sand particles, one each second, for the next 50 seconds
+let actionSpawnSandParticle = SKAction.run(spawnSand)
+let actionWait = SKAction.wait(forDuration: 0.5)
+let sequenceSpawnThenWait = SKAction.sequence([actionSpawnSandParticle, actionWait])
+let actionRepeatlyAddSand = SKAction.repeat(sequenceSpawnThenWait, count: 100)
+scene.run(actionRepeatlyAddSand)
 
 /*:
  ### Exercises
